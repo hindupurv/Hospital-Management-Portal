c224097f4549878f2bd149be11e50c6cdb6dfcd2
hindupurv <hindupur.v@northeastern.edu>
2022-10-27 13:53:20 -0400

    More portals added

---
+++ HospitalManagementPortal/src/hospitalmanagementportal/City.java
@@ -0,0 +1,17 @@
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package hospitalmanagementportal;
+
+/**
+ *
+ * @author ASUS
+ */
+public class City {
+    
+    String name = "";
+    Community community = new Community();
+    
+}
